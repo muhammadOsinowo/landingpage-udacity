a1dee7d275151cab83eacfc2e434312c1b731499
Muhammad <muhammadosinowo91@gmail.com>
2023-01-10 11:14:46 +0200

    seventh commit

--- project/js/app.js
+++ project/js/app.js
@@ -13,8 +13,6 @@ let timer = null;
 
 // # end global variable
 
-// # start helper functions
-
 // add link to the list
 
 let AddLinks = (ele) => {
@@ -45,72 +43,57 @@ let scroll = (e) => {
 };
 
 
-// add active class to the section in the viewport
+//the section being viewed
+const offset = (section) => {
+  return value = Math.floor(section. getBoundingClientRect().top);
+};
 
-const shower = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('your-active-class');
-      document.querySelector(`.${entry.target.id}`).classList.add('active');
-    } else {
-      entry.target.classList.remove('your-active-class');
-      document.querySelector(`.${entry.target.id}`).classList.remove('active');
-    }
-  });
-});
 
-// open and close the navbar menu
-const OcNav = () => {
-    list.classList.toggle('open');
-    toggler.classList.toggle('close');
+// remove the active class
+const removeActive = (section) => {
+  section.classList.remove('your-active-class');
+  section.style.cssText = 'background-color : rgba(255, 255, 255, 0.187)';
+};
+
+//adding the active class
+const addActive = (conditional, section) => {
+  if(conditional){
+    section.classList.add('your-active-class');
+    section.style.cssText = 'background-color : grey';
   };
-  
+};
 
-//this function for hide the header
+//implementing the function
 
+const sectionActivate = () =>  {
+  sections.forEach(section =>{
+    const elementOffset = offset(section);
 
-// # end helper functions
+    inviewport = () => elementOffset < 150 && elementOffset >= -150;
 
-/*
-when the function load this function will
- - add spans -important part for animation- to all the elements witch have btn-hover class
- - add svg to the h2 on every element has class landing__container
-  - for know this section can be collapsed
+    removeActive(section);
+    addActive(inviewport(), section);
+  });
+};
 
-*/
+window.addEventListener('scroll',sectionActivate);
 
 // looping in the sections and make a link to each section
 sections.map(AddLinks);
 
 // add id 'toggler' to the toggle btn
-
 toggler.setAttribute('id', 'toggler');
 
 // add bars to toggler btn
-
 for (let i = 0; i < 3; i++) {
   bar.classList.add('bar');
   toggler.appendChild(bar);
 }
 
 // add the toggler btn to the navbar
-
 document.querySelector('.navbar__menu').appendChild(toggler);
 
-// open and close navbar menu when user click the toggler btn
-
-toggler.addEventListener('click', OcNav);
-
-// add class active to the section on the view port
-
-document.querySelectorAll('section').forEach((ele) => shower.observe(ele));
-
-// hide the navbar when the scroll stop
-
-// window.addEventListener('scroll', hiddenNavbar, false);
-
 // show navbar when user scroll scroll
-
 window.onscroll = () => {
   document.querySelector('header').style = 'transform: translateY(0);';
 };
